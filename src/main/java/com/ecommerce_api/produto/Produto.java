211c84ad681c5415b6e9df493db80224f8a53028
Allan José <allanjose269@gmail.com>
2024-07-30 07:49:49 -0300

    E-01 - Criação da Entidade Produto

---
+++ src/main/java/com/ecommerce_api/produto/Produto.java
@@ -0,0 +1,28 @@
+package com.ecommerce_api.produto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double preco;
+
+}
