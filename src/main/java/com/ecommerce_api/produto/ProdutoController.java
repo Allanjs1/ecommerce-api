c00e7aa3c2fd81f4e47faa59cc2d3ccb42a95d18
Allan José <allanjose269@gmail.com>
2024-07-30 15:24:54 -0300

    E-01 - ProdutoController_Endpoint para Adicionar um Novo Produto

---
+++ src/main/java/com/ecommerce_api/produto/ProdutoController.java
@@ -0,0 +1,23 @@
+package com.ecommerce_api.produto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<Produto> registerProduto(@RequestBody Produto produto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.registerProduto(produto));
+    }
+
+}
