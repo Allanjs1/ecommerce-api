f03dcc1826b8e849103dbe93d856f421a7743ce5
Allan José <allanjose269@gmail.com>
2024-08-03 17:03:54 -0300

    E-02 - Criação da Classe PedidoService

---
+++ src/main/java/com/ecommerce_api/pedido/PedidoService.java
@@ -0,0 +1,45 @@
+package com.ecommerce_api.pedido;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository repository;
+
+    public Pedido register(Pedido pedido) {
+        return repository.save(pedido);
+    }
+
+    public List<Pedido> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Pedido> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Pedido update(Pedido pedido, Long id) {
+        pedido.setId(id);
+        return repository.save(pedido);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Double calcularValorComDesconto(Double valorTotal) {
+        if (valorTotal > 200) {
+            return valorTotal * 0.9;
+
+        } else {
+            return valorTotal;
+        }
+    }
+
+}
