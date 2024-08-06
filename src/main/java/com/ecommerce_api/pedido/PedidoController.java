86ee71028b9676b5f71ac0332f4cb3e8a4a35469
Allan José <allanjose269@gmail.com>
2024-08-06 20:27:43 -0300

    E-02 - PedidoController_ Endpoint Criado para Listar Todos os Pedidos

--- src/main/java/com/ecommerce_api/pedido/PedidoController.java
+++ src/main/java/com/ecommerce_api/pedido/PedidoController.java
@@ -1,7 +1,11 @@
 package com.ecommerce_api.pedido;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +17,14 @@ public class PedidoController {
     private PedidoService pedidoService;
 
     @PostMapping
-    public ResponseEntity<Double> criarPedido(@RequestBody Pedido pedido) {
+    public ResponseEntity<Double> adicionarPedido(@RequestBody Pedido pedido) {
         double valorComDesconto = pedidoService.calcularValorComDesconto(pedido.getValorTotal());
         return ResponseEntity.ok(valorComDesconto);
-
     }
 
+    @GetMapping
+    public ResponseEntity<List<Pedido>> listarPedidos() {
+        return ResponseEntity.status(HttpStatus.OK).body(pedidoService.listarPedidos());
+
+    }
 }
