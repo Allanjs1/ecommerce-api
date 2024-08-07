72588d86edc74842add60a834fcd713c4c547c81
Allan José <allanjose269@gmail.com>
2024-08-06 23:28:47 -0300

    E-02 - PedidoController_ Endpoint para Obter Detalhes de um Produto Específico

--- src/main/java/com/ecommerce_api/pedido/PedidoController.java
+++ src/main/java/com/ecommerce_api/pedido/PedidoController.java
@@ -1,11 +1,13 @@
 package com.ecommerce_api.pedido;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,4 +29,13 @@ public class PedidoController {
         return ResponseEntity.status(HttpStatus.OK).body(pedidoService.listarPedidos());
 
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pedido> getPedidoId(@PathVariable Long id) {
+        Optional<Pedido> pedido = pedidoService.findById(id);
+        return pedido.map(ResponseEntity::ok).orElseGet(()
+                -> ResponseEntity.notFound().build());
+
+    }
+
 }
